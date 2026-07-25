91e2f631013998cbd0dc915cf75b681aa28ddd35
Alshainen <alshainschatten@gmail.com>
2026-07-25 19:48:18 +0700

    Learn JavaScript Day 1 (Variables)

---
+++ Variables/web/variablesWeb.js
@@ -0,0 +1,9 @@
+let namaKu = `Rizky`;
+let umurKu = 19;
+let tabunganKu = 124.49;
+
+document.getElementsByTagName("p")[0].textContent = `perkenalkan namaku ${namaKu},`
+document.getElementsByTagName("p")[1].textContent = `umurku ${umurKu} tahun,`
+document.getElementsByTagName("p")[2].textContent = `Saat ini tabunganku berjumlah $${tabunganKu}.`
+// Sama aja kayak yang di console.log.
+// bedanya ini untuk ditampilkan ke webnya langsung, bukan cuman dari console.
